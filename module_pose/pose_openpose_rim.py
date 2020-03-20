a89542b0ee1433543b3222417ae2e5ebd2500661
sugartom <sugartom.hu@gmail.com>
2020-03-20 15:10:47 -0700

    exporrted to Rim

--- module_pose/pose_openpose_rim.py
+++ module_pose/pose_openpose_rim.py
@@ -3,6 +3,7 @@ from tensorflow_serving.apis import predict_pb2
 from tensorflow.python.framework import tensor_util
 import cv2
 import tensorflow as tf
+import pickle
 
 import sys
 sys.path.append('/home/yitao/Documents/fun-project/tensorflow-related/tf-pose-estimation/')
@@ -60,7 +61,7 @@ class PoseOpenpose:
       # next_request.inputs["client_input"].CopyFrom(
       #   tf.make_tensor_proto(self.image))
       next_request.inputs["FINAL"].CopyFrom(
-        tf.make_tensor_proto(pickle.dumps(self.humans)))
+        tf.make_tensor_proto("OK"))
     else:
       next_request = dict()
       # next_request["client_input"] = self.image
